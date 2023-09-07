cb19ec1b069ee4806bd8dd8b82e147345542429d
Ahmed Gürkan Özçelik <ahmetgozcelik@gmail.com>
2023-09-07 20:22:20 +0300

    Boost has been added to the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f; // Uygulanacak tork kuvveti
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+
     Rigidbody2D rb2d; // Torku Rigidbody'e uygulayacaðýmýz için rigidbody'i tanýmladýk.
+    SurfaceEffector2D surfaceEffector2D; // SurfaceEffector'u tanýmladýk.
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RotatePlayer()
     {
         //Sol ok tuþuna basýlýrsa torqueAmount kadar tork uygula.
         if (Input.GetKey(KeyCode.A))
@@ -19,9 +31,21 @@ public class PlayerController : MonoBehaviour
             rb2d.AddTorque(torqueAmount);
         }
         //Sað ok tuþuna basýlýrsa torqueAmount kadar tork uygula. Zýt yön olmasýna dikkat!
-        if (Input.GetKey(KeyCode.D)) 
+        if (Input.GetKey(KeyCode.D))
         {
             rb2d.AddTorque(-torqueAmount);
+        }
+    }// Player döndürme - tork ile
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
